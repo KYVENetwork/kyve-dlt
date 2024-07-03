49896e36238e93c69992f880f588622aedcc2260
christopherbrumm <christopherbrumm2000@gmail.com>
2024-07-03 17:10:40 +0200

    fix: catch if initial bundles are empty

--- loader/collector/bundles_collector.go
+++ loader/collector/bundles_collector.go
@@ -62,6 +62,11 @@ func (s Source) FetchBundles(offset int64, handler func(bundles []Bundle, err er
 		handler(nil, fmt.Errorf("initial bundle request failed: %s", responseError.Error()))
 	}
 
+	if len(initialBundles) == 0 {
+		handler(nil, fmt.Errorf("could not find any bundles yet; from-bundle-id too high or interval too short"))
+		return
+	}
+
 	highestBundleId, err := strconv.ParseInt(initialBundles[len(initialBundles)-1].Id, 10, 64)
 	if err != nil {
 		handler(nil, fmt.Errorf("malformed bundle response, invalid bundle-id: %s", err.Error()))
@@ -116,7 +121,7 @@ func (s Source) FetchBundles(offset int64, handler func(bundles []Bundle, err er
 		handler(bundles, nil)
 
 		if nextKey == "" {
-			handler(nil, errors.New("invalid nextKey returned or end was reached early"))
+			logger.Info().Msg("reached last bundle")
 			return
 		}
 		paginationKey = nextKey
